e6b3fd28769e88c8edce097a5a44fbe45709dd23
David Guttman <david@davidguttman.com>
2018-03-05 08:56:36 -0800

    feat: add echo route

--- test/routes.js
+++ test/routes.js
@@ -83,19 +83,28 @@ tape('should get stream', function (t) {
       t.end()
     })
 })
-
 tape('should get reverse', function (t) {
   var expected = {
     input: 'stringtoreverse',
-    output: 'esreverotgnirts'
-  }
+    output: 'esreverotgnirts',
+  };
 
-  var url = '/reverse/' + expected.input
-  servertest(server(), url, {encoding: 'json'}, function (err, res) {
-    t.ifError(err, 'no error')
+  var url = '/reverse/' + expected.input;
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.ifError(err, 'no error');
 
-    t.equal(res.statusCode, 200, 'correct statusCode')
-    t.deepEqual(res.body, expected, 'values should match')
-    t.end()
-  })
-})
+    t.equal(res.statusCode, 200, 'correct statusCode');
+    t.deepEqual(res.body, expected, 'values should match');
+    t.end();
+  });
+});
+tape('should get echo', function (t) {
+  var url = '/echo?one=1&two=2';
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.ifError(err, 'no error');
+
+    t.equal(res.statusCode, 200, 'correct statusCode');
+    t.deepEqual(res.body, { one: '1', two: '2' }, 'values should match');
+    t.end();
+  });
+});
